b9f4e3aeb3f0a71e25e7723f71a046f4b484699e
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-26 23:10:05 +0200

    handle cloudinary archive

---
+++ pages/api/companies/[id].js
@@ -0,0 +1,19 @@
+
+var cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: "eventjuicer",
+    api_key: `${process.env.CLOUDINARY_API_KEY}`,
+    api_secret: `${process.env.CLOUDINARY_API_SECRET}`
+})
+
+export default async function handler(req, res) {
+
+    const { id } = req.query
+    
+    const query = await cloudinary.utils.download_folder(`teh20/${id}`, {
+        target_public_id: `teh20_${id}_photos`,
+        flatten_folders: true
+    });
+    
+    res.status(200).json(query)
+}
